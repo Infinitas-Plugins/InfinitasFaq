6db128a659ab658734fd0666bac5bbdef70c5ccc
dogmatic69 <dogmatic69@gmail.com>
2012-03-15 00:29:47 +0000

    making the js change the url anchor and scroll to the faq that is opened. histroy back / forward buttons now work through the faq tabs

--- webroot/js/infinitas_faq.js
+++ webroot/js/infinitas_faq.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
 	$('.answer-wrapper').hide();
+	if(location.hash != '') {
+		$(location.hash).show();
+	}
 
+	/**
+	 * When clicking a link change the hash in the url 
+	 */
 	$('.question').live('click', function() {
-		$(this).next().slideToggle();
+		location.hash = $(this).next().attr('id');
+	});
+	
+	/**
+	 * Catch location.hash changes and show/hide according to what is selected
+	 */
+	$(window).bind('hashchange',function(event) {
+		$('.answer-wrapper').hide();
+		var hash = location.hash.replace('#','');
+		if(hash == '') {
+			$(window).scrollTop(0);
+			return false;
+		}
+		
+		$(location.hash).slideToggle();
+		$('html,body').animate({scrollTop: $(location.hash).parent().offset().top}, 'slow');
+		return false;
 	});
 });
