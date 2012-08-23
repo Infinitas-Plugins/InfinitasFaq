f0e0a53f6769b160963f03bab509bef12d2821cd
dogmatic69 <dogmatic69@gmail.com>
2012-08-23 02:16:46 +0100

    adding new fixtures

---
+++ Test/Fixture/InfinitasFaqContentFixture.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @brief fixture file for InfinitasFaqContent tests.
+ *
+ * @package InfinitasFaq.Fixture
+ * @since 0.9b1
+ */
+class InfinitasFaqContentFixture extends CakeTestFixture {
+	public $name = 'InfinitasFaqContent';
+
+	public $fields = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'comment_count' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index'),
+		'views' => array('type' => 'integer', 'null' => false, 'default' => '0', 'key' => 'index'),
+		'rating' => array('type' => 'float', 'null' => false, 'default' => '0'),
+		'rating_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'ordering_id' => array('type' => 'integer', 'null' => false, 'default' => '1'),
+		'ordering' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'active' => array('column' => 'active', 'unique' => 0),
+			'most_views' => array('column' => array('views', 'id'), 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $records = array(
+	);
+}
